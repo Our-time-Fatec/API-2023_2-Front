e8496517d4a7c14047f89911e36e442f91c31acd
lucasdwn <lfcosta0804@gmail.com>
2023-09-15 18:24:26 -0300

    Melhorando a verificação do token

--- src/services/auth.tsx
+++ src/services/auth.tsx
@@ -1,56 +1,72 @@
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import api from "./api";
 import { useEffect } from "react";
 import jwtDecode from "jwt-decode";
 
 interface User {
-    username: string;
-    token: string;
+  username: string;
+  token: string;
 }
 interface DecodedToken {
-  userId: string; // Defina a estrutura do token JWT
+  userId: string;
   exp: number;
 }
 
 const login = async (username: string, password: string): Promise<User> => {
-    const response = await api.post(`/user/login`, { username, password });
-    return response.data;
+  const response = await api.post(`/user/login`, { username, password });
+  return response.data;
 };
 
 const logout = (): void => {
-    localStorage.removeItem('user');
+  localStorage.removeItem('user');
 };
 
 function isTokenExpired(tokenJson: string | null): boolean {
   if (!tokenJson) {
-      return true;
+    return true;
   }
   const decodedToken = jwtDecode<DecodedToken>(tokenJson);
 
   if (!decodedToken.exp) {
-      return false; 
+    return false;
   }
-  console.log('verificou')
+
   const currentTimestamp = Math.floor(Date.now() / 1000);
-  
+
   return decodedToken.exp < currentTimestamp;
 }
 
 function AuthMiddleware({ children }: { children: React.ReactNode }) {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const isAuthenticated = !!localStorage.getItem('user');
 
-    useEffect(() => {
+  // Executar verificação de token após mudança de rota.
+  useEffect(() => {
+    const tokenJson = localStorage.getItem('user');
+    if (isTokenExpired(tokenJson)) {
+      localStorage.removeItem('user');
+    }
+  }, [location.pathname, isAuthenticated]);
+
+  // Executar verificação de token de 1 em 1 hora
+  useEffect(() => {
+    const intervalId = setInterval(() => {
       const tokenJson = localStorage.getItem('user');
       if (isTokenExpired(tokenJson)) {
         localStorage.removeItem('user');
+        navigate(0);
       }
-    }, []);
-  
-    return children as JSX.Element;
-  }
-  
+    }, 60 * 60 * 1000); // 1 Hora em milissegundos
+
+    return () => clearInterval(intervalId);
+  }, [navigate]);
+
+  return <>{children}</>;
+}
 
 export default {
-    login,
-    logout,
-    AuthMiddleware
+  login,
+  logout,
+  AuthMiddleware
 };
