74cd9cfc17890844ad276c70eeaf1171cd1322c6
lucasdwn <lfcosta0804@gmail.com>
2023-09-24 23:25:33 -0300

    Adicionando listagem de bikes alugadas

--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -28,19 +28,37 @@ function Inicio() {
                 <NavBar />
             </header>
             <main className="main-container">
-                <h1 className="mt-3">Bicicletas disponiveis</h1>
-                <div className="bike mx-4 d-flex flex-wrap align-items-center justify-content-center gap-3">
-                    {
-                        bicicletas && bicicletas.filter((i) => !i.isAlugada).map((i) => {
-                            return (
+                <div className="d-flex flex-column align-items-center justify-content-center mt-3 gap-2">
+                    <h1 className="text-success">Bicicletas disponiveis</h1>
+                    <div className="bike mx-4 d-flex flex-wrap align-items-center justify-content-center gap-3 mt-3">
+                        {
+                            bicicletas && bicicletas.filter((i) => !i.isAlugada).map((i) => {
+                                return (
 
-                                <div className="bike" key={i.id}>
-                                    <CardBike id={i.id} marca={i.marca?.nome} modalidade={i.modalidade?.nome} foto={i.fotos && i.fotos[0]?.url} descricao={i.descricao} valorDia={i.valorDia} valorHora={i.valorHora} donoId={i.donoId} />
-                                </div>
+                                    <div className="bike" key={i.id}>
+                                        <CardBike id={i.id} marca={i.marca?.nome} modalidade={i.modalidade?.nome} foto={i.fotos && i.fotos[0]?.url} descricao={i.descricao} valorDia={i.valorDia} valorHora={i.valorHora} donoId={i.donoId} />
+                                    </div>
 
-                            )
-                        })
-                    }
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+                <div className="d-flex flex-column align-items-center justify-content-center mt-5 gap-2">
+                    <h1 className="text-danger">Bicicletas alugadas</h1>
+                    <div className="bike mx-4 d-flex flex-wrap align-items-center justify-content-center gap-3 mt-3">
+                        {
+                            bicicletas && bicicletas.filter((i) => i.isAlugada).map((i) => {
+                                return (
+
+                                    <div className="bike" key={i.id}>
+                                        <CardBike id={i.id} marca={i.marca?.nome} modalidade={i.modalidade?.nome} foto={i.fotos && i.fotos[0]?.url} descricao={i.descricao} valorDia={i.valorDia} valorHora={i.valorHora} donoId={i.donoId} isAlugada={i.isAlugada} />
+                                    </div>
+
+                                )
+                            })
+                        }
+                    </div>
                 </div>
             </main>
         </div>
