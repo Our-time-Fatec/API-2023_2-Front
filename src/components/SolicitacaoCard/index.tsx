4b69201de6534e0a1a5562ab9ccded3cb0b86fb2
lucasdwn <lfcosta0804@gmail.com>
2023-11-22 14:05:10 -0300

    adicionando botões de aceitar e rejeitar solicitação

--- src/components/SolicitacaoCard/index.tsx
+++ src/components/SolicitacaoCard/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "react-bootstrap";
 import DiaouHora from "../../Enums/DiaouHora";
 import Bicicleta from "../../interfaces/Bicicleta";
 import User from "../../interfaces/User";
@@ -14,13 +15,30 @@ interface SoliCard {
 }
 
 function SolicitacaoCard({ idSolicitacao, idLocatario, idBicicleta, isRespondido, isAceito, DiaouHora, locatario, bicicleta }: SoliCard) {
+
+    const acceptSolicitacao = async () => {
+        alert('Solicitação aceita')
+    }
+
+    const rejectSolicitacao = async () => {
+        alert('Solicitação Rejeitada')
+    }
+
     return (
         <div className="card-bike">
             <div className="card-title roboto-negrito text-black">Solicitação: {idSolicitacao}</div>
+            <p className="text-green">Aguardando resposta </p>
             <span className="text-black roboto-regular">Locatário: {locatario?.username}</span><br></br>
             <span className="text-black roboto-regular">Bicicleta: {bicicleta?.marca?.nome} - {bicicleta?.modalidade?.nome}</span><br></br>
             <span className="text-black roboto-regular">Tipo De Solicitacao: {DiaouHora}</span><br></br>
-            <p className="text-green">Aguardando resposta </p>
+            <div className="d-flex gap-2">
+                <Button variant="primary" className='mt-2' onClick={acceptSolicitacao}>
+                    Aceitar
+                </Button>
+                <Button variant="danger" className='mt-2' onClick={rejectSolicitacao}>
+                    Rejeitar
+                </Button>
+            </div>
         </div>
     );
 }
